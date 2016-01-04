8dbcfab14b1904d173c38e0d46583346e86256b6
Brian R. Bondy <netzen@gmail.com>
2016-01-04 10:04:18 -0500

    Update abp-filter-parser-cpp usage for new matches interface

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var BloomFilter = require('bloom-filter-cpp').BloomFilter
 var HashSet = require('hashset-cpp').HashSet
-var ABPFilterParser = require('abp-filter-parser-cpp').ABPFilterParser
+var ABPFilterParserLib = require('abp-filter-parser-cpp')
+var ABPFilterParser = ABPFilterParserLib.ABPFilterParser
+var FilterOptions = ABPFilterParserLib.FilterOptions
 
 ;(function bloomFilterTest () {
   console.log('Bloom Filter')
@@ -33,9 +35,16 @@ var ABPFilterParser = require('abp-filter-parser-cpp').ABPFilterParser
 ;(function abpFilterParserTest() {
   console.log('\nABPFilterParser')
   var parser = new ABPFilterParser()
-  parser.parse('/public/ad/*')
-  var b1 = parser.matches('http://www.brianbondy.com/public/ad/some-ad')
-  var b2 = parser.matches('http://www.brianbondy.com/public/ad2/some-ad')
+  parser.parse('/public/ad/*$domain=slashdot.org')
+  parser.parse('/public/ad3/*$script')
+  var b1 = parser.matches('http://www.brianbondy.com/public/ad/some-ad', FilterOptions.script, 'slashdot.org')
+  var b2 = parser.matches('http://www.brianbondy.com/public/ad/some-ad', FilterOptions.script, 'digg.com')
+  var b3 = parser.matches('http://www.brianbondy.com/public/ad2/some-ad', FilterOptions.script, 'slashdot.org')
+  var b4 = parser.matches('http://www.brianbondy.com/public/ad3/some-ad', FilterOptions.script, 'slashdot.org')
+  var b5 = parser.matches('http://www.brianbondy.com/public/ad3/some-ad', FilterOptions.image, 'slashdot.org')
   console.log('public/ad/* should match b1.  Actual: ', b1)
   console.log('public/ad/* should not match b2.  Actual: ', b2)
+  console.log('public/ad/* should not match b3.  Actual: ', b3)
+  console.log('public/ad/* should match b4.  Actual: ', b4)
+  console.log('public/ad/* should not match b5.  Actual: ', b5)
 })()
